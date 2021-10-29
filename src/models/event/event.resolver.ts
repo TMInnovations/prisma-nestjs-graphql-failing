0466f32ead2768bb3ccad22f2536ec95985c08a4
Thomas Moser <tm@tminnovations.at>
2021-10-29 09:41:39 +0200

    removed events who resolver

--- src/models/event/event.resolver.ts
+++ src/models/event/event.resolver.ts
@@ -1,28 +1,15 @@
-import {
-  Args,
-  Mutation,
-  Parent,
-  Query,
-  ResolveField,
-  Resolver,
-} from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import {
   Event,
   EventCreateInput,
   EventWhereUniqueInput,
   FindManyEventArgs,
   UpdateOneEventArgs,
-  User,
 } from '@prisma/client/nestjs-graphql';
 import { EventService } from './event.service';
 @Resolver(() => Event)
 export class EventResolver {
   constructor(private readonly eventService: EventService) {}
-
-  @ResolveField(() => User, { name: 'who' })
-  Who(@Parent() event: Event) {
-    return this.eventService.findOne({ id: event.id }).who();
-  }
 
   // CRUD
 
